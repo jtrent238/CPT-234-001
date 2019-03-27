056ee06db8bb365aef1aed3164696080907256a2
Trent Patterson <jtrent238@outlook.com>
2019-03-27 19:12:52 -0400

    3272019712

--- Projects/Grade Curve/Grade Curve/Grade Curve.cpp
+++ Projects/Grade Curve/Grade Curve/Grade Curve.cpp
@@ -1,9 +1,9 @@
-// Programmer:		Your Name
-// Date:			Date
-// Program Name:	The name of the program
-// Chapter:			Chapter # - Chapter name
-// Description:		2 complete English sentences describing what the program does,
-//					algorithm used, etc.
+// Programmer:		Jonathan Trent Patterson
+// Date:			3/17/2019
+// Program Name:	Grade Curve
+// Chapter:			Chapter 5 - Arrays
+// Description:		This program ask user for x number of scores then caculates the Grade Curve.
+//					It then outputs the new grades.
 
 #define _CRT_SECURE_NO_WARNINGS // Disable warnings (and errors) when using non-secure versions of printf, scanf, strcpy, etc.
 #include <stdio.h> // Needed for working with printf and scanf
@@ -31,6 +31,7 @@ int main(void)
 
 		// *** Your program goes here ***
 
+	// Get input from user and populate array of stuGrade
 		for (int stuNum = STU_START; stuNum < NUM_STUDENTS + 1; stuNum++) {
 			printf("Enter the score for student #%d: ", stuNum);
 			scanf("%lf", &stuGrade[stuNum]);
@@ -45,22 +46,26 @@ int main(void)
 		classTotal += stuGrade[stuNum];
 	}
 
-	classAvg = classTotal / NUM_STUDENTS;
-	curveScore = 75 - classAvg;
+	classAvg = classTotal / NUM_STUDENTS; // Caculates the class Average
+	curveScore = 75 - classAvg;	// Caculates the curve score
 
 	printf("\n"); // Blank Line
 
+	// Prints the output
 	printf("The average of the scores was: %.1lf" "\n", classAvg);
 	printf("Each student will have %.1lf added to their score." "\n", curveScore);
 
 	printf("\n"); // Blank Line
 
+	//Checks if classAvg is in range
 	if (classAvg > MIN_AVG || classAvg < MAX_AVG) {
 
 	}
 
+	// Prints new score header
 	printf("The new scores are:" "\n");
-
+	
+	// Prints the new scores
 	for (int stuNum = STU_START; stuNum < NUM_STUDENTS + 1; stuNum++) {
 		stuGradeNew[stuNum] = stuGrade[stuNum] + curveScore;
 		printf("The score for student #%d is: %.1lf" "\n", stuNum, stuGradeNew[stuNum]);
